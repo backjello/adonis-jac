c31aab91305822e7467fd5327099f00f118eb937
backjello <jackbello97@gmail.com>
2024-02-02 14:14:29 +0100

    post model

---
+++ app/Models/Post.ts
@@ -0,0 +1,24 @@
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from 'luxon'
+
+export default class Post extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public title: string
+
+  @column()
+  public body: string
+
+  @column()
+  public userId: number
+
+
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
